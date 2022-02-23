cf6bf1d124cbeba8286778bc240b12f86d0c31c0
TENIKALER SMITH <tenikaler.smith@utp.ac.pa>
2022-02-22 21:54:10 -0500

    Variables de enterno para Token de Login

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,11 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  HOST: 'http://localhost:8080',
+  TOKEN_AUTH_EMAIL: 'ADMIN@test.com',
+  TOKEN_AUTH_PASSWORD: 'tienda1234',
+  TOKEN_NAME: 'token'
 };
 
 /*
