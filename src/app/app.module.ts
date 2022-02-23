076c13454659328b093addc5026c34b0f3069c20
TENIKALER SMITH <tenikaler.smith@utp.ac.pa>
2022-02-22 21:56:00 -0500

    Refactorización de los import de Angular Material y el componente Menu se paso a la Layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,36 +1,53 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {FormsModule} from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {MatToolbarModule} from '@angular/material/toolbar';
-import {MatSidenavModule} from '@angular/material/sidenav';
-import {MatIconModule} from '@angular/material/icon';
-import {MatButtonModule} from '@angular/material/button';
-import {MatMenuModule} from '@angular/material/menu';
-import {MatDividerModule} from '@angular/material/divider';
-import { CategoriasComponent } from './pages/categorias/categorias.component';
-import { UsuariosComponent } from './pages/usuarios/usuarios.component';
 
+import { LoginComponent } from './pages/login/login.component';
+import { MatButtonModule } from '@angular/material/button';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatIconModule } from '@angular/material/icon';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatCardModule } from '@angular/material/card';
+import { MatToolbarModule } from '@angular/material/toolbar';
+
+import { environment } from 'src/environments/environment';
+import { HttpClientModule } from '@angular/common/http';
+
+export function tokenGetter(){
+  return sessionStorage.getItem(environment.TOKEN_NAME);
+}
 
 @NgModule({
   declarations: [
     AppComponent,
-    CategoriasComponent,
-    UsuariosComponent
+    LoginComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatToolbarModule,
-    MatSidenavModule,
-    MatIconModule,
-    MatButtonModule,
+    FormsModule,  
+
+    MatButtonModule,    
     MatMenuModule,
-    MatDividerModule
+    MatIconModule,
+    MatDividerModule,
+    MatFormFieldModule,
+    MatCardModule,
+    MatToolbarModule,
+    HttpClientModule
+
+  
   ],
+  exports: [
+    
+  ],
+  
   providers: [],
   bootstrap: [AppComponent]
 })
