4a9bd3d2235a2b9ed5cac267fba6b73e02868ee4
TENIKALER SMITH <tenikaler.smith@utp.ac.pa>
2022-02-23 00:33:30 -0500

    rutas para el componente Usuario, categorias...

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LayoutComponent } from './pages/layout/layout.component';
 import { LoginComponent } from './pages/login/login.component';
 
 
 const routes: Routes = [
-  
-  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: '', redirectTo: 'Login', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
-  {path: 'pages',
-  loadChildren: () => import('./pages/pages.module').then(m => m.PagesModule)
 
-}
+
+  {path: 'pages',
+  component: LayoutComponent,
+  loadChildren: () => import('./pages/pages.module').then(m => m.PagesModule)},
+
+  {path: '**', redirectTo: 'login', pathMatch: 'full'},
+
+
   
 ];
 
--- src/app/pages/pages-routing.module.ts
+++ src/app/pages/pages-routing.module.ts
@@ -1,10 +1,13 @@
-import { CommonModule } from '@angular/common';
-import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CategoriasComponent } from './categorias/categorias.component';
+import { UsuariosComponent } from './usuarios/usuarios.component';
 //import { GuardService } from '../_service/guard.service';
 
 export const routes: Routes = [
+    { path : '', redirectTo: 'usuarios', pathMatch: 'full' },
+    { path : 'usuarios', component:	UsuariosComponent },
+    { path  : 'categorias', component:	CategoriasComponent },
     // { path: 'inicio', component: InicioComponent, canActivate: [GuardService] },   
 ]
 
