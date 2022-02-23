db9c956d067fd00d6e5e76191fe6d65fb5712039
Tomas Padilla <padillatomasagustin@gmail.com>
2022-02-23 20:44:13 +0100

    Homepage - Banner & Categories OK

--- src/app/pages/categorias/categorias.component.ts
+++ src/app/pages/categorias/categorias.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { Category } from 'src/app/_model/category';
+import { CategoryService } from 'src/app/_service/category.service';
 
 @Component({
   selector: 'app-categorias',
   templateUrl: './categorias.component.html',
-  styleUrls: ['./categorias.component.scss']
+  styleUrls: ['./categorias.component.scss'],
 })
 export class CategoriasComponent implements OnInit {
+  categories: Category[];
 
-  constructor() { }
+  constructor(private categoryService: CategoryService) {}
 
-  ngOnInit(): void {
+  ngOnInit() {
+    this.listCategories();
   }
 
+  listCategories() {
+    this.categoryService.getCategories().subscribe((data) => {
+      this.categories = data;
+      console.log(this.categories);
+    });
+  }
 }
