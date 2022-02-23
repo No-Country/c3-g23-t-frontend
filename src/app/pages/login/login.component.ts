d9dd9fd9cd2813905049655615d476829e1eaac3
TENIKALER SMITH <tenikaler.smith@utp.ac.pa>
2022-02-22 21:50:03 -0500

    creacion de componente login

---
+++ src/app/pages/login/login.component.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import {LoginService } from 'src/app/_service/login.service';
+import {environment} from 'src/environments/environment';
+
+
+@Component({
+    selector: 'app-login',
+    templateUrl: './login.component.html',
+    styleUrls: ['./login.component.scss']
+})
+
+export class LoginComponent implements OnInit {
+    
+    email: string;
+    password: string;
+    mensaje: string;
+    error: string;
+    
+    constructor(
+        private router: Router, 
+        private loginService: LoginService
+        ) { }
+
+    ngOnInit(): void {
+    }
+
+    login(): void {
+        this.loginService.login(this.email, this.password).subscribe(data =>{
+            //sessionStorage.setItem(environment.TOKEN_NAME, data.access_token);
+            this.router.navigate(['pages/usuarios']);
+        });
+    }
+    
+    ngAfterViewInit(){
+        (window as any).initialize();
+    }
+
+}
