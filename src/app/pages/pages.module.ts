c58a5fcade0dd45fe943106e593004175803dd32
Tomas Padilla <padillatomasagustin@gmail.com>
2022-02-25 11:37:51 +0100

    Navigation - Custom Layout component

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -1,6 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
-import  {NgModule} from '@angular/core';
+import { NgModule } from '@angular/core';
 import { MaterialModule } from '../material/material.module';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { PagesRoutingModule } from './pages-routing.module';
@@ -11,27 +11,26 @@ import { UsuariosComponent } from './usuarios/usuarios.component';
 import { HomeComponent } from './home/home.component';
 import { Not403Component } from './not403/not403.component';
 import { Not404Component } from './not404/not404.component';
-
+import { LayoutCustomComponent } from './layout-custom/layout-custom.component';
 @NgModule({
-    imports: [
-        MaterialModule,
-        CommonModule,
-        HttpClientModule,
-        ReactiveFormsModule,
-        FormsModule,
-        PagesRoutingModule
-    ],
-    exports: [],
-    declarations: [
-        UsuariosComponent,
-        CategoriasComponent,
-        LayoutComponent,
-        HomeComponent,
-        Not403Component,
-        Not404Component
-    ],
-    providers: [],
-
+  imports: [
+    MaterialModule,
+    CommonModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+    FormsModule,
+    PagesRoutingModule,
+  ],
+  exports: [],
+  declarations: [
+    UsuariosComponent,
+    CategoriasComponent,
+    LayoutComponent,
+    HomeComponent,
+    Not403Component,
+    Not404Component,
+    LayoutCustomComponent,
+  ],
+  providers: [],
 })
-
-export class PagesModule { }
+export class PagesModule {}
