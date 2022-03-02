f6039935b0e15250f92a3f927ab8a06fcffee7ed
Tomas Padilla <padillatomasagustin@gmail.com>
2022-03-02 19:49:13 +0100

    Register - Falta Validations y Error

--- src/app/pages/register-custom/register-custom.component.ts
+++ src/app/pages/register-custom/register-custom.component.ts
@@ -1,6 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
 import { Router } from '@angular/router';
 import { RegisterCustomService } from 'src/app/_service/register-custom.service';
+import { CustomValidators } from 'src/app/_validators/custom-validators';
 
 @Component({
   selector: 'app-register-custom',
@@ -8,38 +15,68 @@ import { RegisterCustomService } from 'src/app/_service/register-custom.service'
   styleUrls: ['./register-custom.component.scss'],
 })
 export class RegisterCustomComponent implements OnInit {
-  username: string;
-  email: string;
-  password: string;
-  name: string;
-  surname: string;
-  country: string;
-  state: string;
-  city: string;
-  imageProfile: string;
+  myForm: FormGroup;
+  myError: number = 200;
 
   constructor(
     private router: Router,
-    private registerService: RegisterCustomService
+    private registerService: RegisterCustomService,
+    private formBuilder: FormBuilder
   ) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.myForm = this.formBuilder.group({
+      username: new FormControl('', [
+        Validators.required,
+        Validators.minLength(2),
+        CustomValidators.notOnlyWhitespace,
+      ]),
+      email: new FormControl('', [
+        Validators.required,
+        Validators.pattern('^[a-zA-Z0-9]+@[a-z0-9.-]+\\.[a-z]{2,8}$'),
+        CustomValidators.notOnlyWhitespace,
+      ]),
+      password: new FormControl('', [
+        Validators.required,
+        Validators.minLength(2),
+        CustomValidators.notOnlyWhitespace,
+      ]),
+      name: new FormControl('', [
+        Validators.required,
+        Validators.minLength(2),
+        CustomValidators.notOnlyWhitespace,
+      ]),
+      surname: new FormControl('', [
+        Validators.required,
+        Validators.minLength(2),
+        CustomValidators.notOnlyWhitespace,
+      ]),
+      country: new FormControl('', [
+        Validators.required,
+        Validators.minLength(2),
+        CustomValidators.notOnlyWhitespace,
+      ]),
+      state: new FormControl('', [
+        Validators.required,
+        Validators.minLength(2),
+        CustomValidators.notOnlyWhitespace,
+      ]),
+      city: new FormControl('', [
+        Validators.required,
+        Validators.minLength(2),
+        CustomValidators.notOnlyWhitespace,
+      ]),
+      imageProfile: new FormControl('', [
+        Validators.required,
+        Validators.minLength(2),
+        CustomValidators.notOnlyWhitespace,
+      ]),
+    });
+  }
 
-  register(): void {
-    this.registerService
-      .register(
-        this.username,
-        this.email,
-        this.password,
-        this.name,
-        this.surname,
-        this.country,
-        this.state,
-        this.city,
-        this.imageProfile
-      )
-      .subscribe((data) => {
-        this.router.navigate(['/pages/usuarios']);
-      });
+  onSubmit(): void {
+    this.registerService.register(this.myForm.value).subscribe((data) => {
+      this.router.navigate(['/pages/usuarios']);
+    });
   }
 }
