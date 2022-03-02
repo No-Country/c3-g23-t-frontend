6a341011398b504fb2fa53300984744fc9a4105b
Tomas Padilla <padillatomasagustin@gmail.com>
2022-03-02 10:12:19 +0100

    QuickFixes - Login, Services, Interfaces, etc

--- src/app/_service/login-custom.service.ts
+++ src/app/_service/login-custom.service.ts
@@ -1,6 +1,8 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
+import { catchError, retry, throwError } from 'rxjs';
+import { Login } from '../_model/login';
 
 @Injectable({
   providedIn: 'root',
@@ -11,6 +13,15 @@ export class LoginService {
   constructor(private httpClient: HttpClient, private router: Router) {}
 
   login(email: string, password: string) {
-    return this.httpClient.post(this.url, { email: email, password: password });
+    return this.httpClient
+      .post<Login>(this.url, { email: email, password: password })
+      .pipe(retry(1), catchError(this.handleError));
   }
+
+  handleError(error: HttpErrorResponse) {
+    return throwError('Algo');
+  }
+
+  // Recibire el OBJETO del FORM (type : Login)
+  // VER INTERFACES / CLASES de tipo Login.
 }
--- src/app/pages/login-custom/login-custom.component.ts
+++ src/app/pages/login-custom/login-custom.component.ts
@@ -10,14 +10,20 @@ import { LoginService } from 'src/app/_service/login-custom.service';
 export class LoginCustomComponent implements OnInit {
   email: string;
   password: string;
+  myError: number;
 
   constructor(private router: Router, private loginService: LoginService) {}
 
   ngOnInit(): void {}
 
   login(): void {
-    this.loginService.login(this.email, this.password).subscribe((data) => {
+    this.loginService.login(this.email, this.password).subscribe((response) => {
       this.router.navigate(['pages/usuarios']);
+      console.log(response);
     });
   }
+
+  // TO-DO
+  // Formulario como Negrito
+  // Me da los atributos, mando el objeto BODY
 }
