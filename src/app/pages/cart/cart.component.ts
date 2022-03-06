fc83cfab36463786323e1532efe5bfa775be13b6
Tomas Padilla <padillatomasagustin@gmail.com>
2022-03-06 18:41:41 +0100

    CartPage

--- src/app/pages/cart/cart.component.ts
+++ src/app/pages/cart/cart.component.ts
@@ -1,15 +1,39 @@
 import { Component, OnInit } from '@angular/core';
+import { CartService } from 'src/app/_service/cart.service';
 
 @Component({
   selector: 'app-cart',
   templateUrl: './cart.component.html',
-  styleUrls: ['./cart.component.scss']
+  styleUrls: ['./cart.component.scss'],
 })
 export class CartComponent implements OnInit {
+  cantItems: number = 2;
+  cartItemsInfo: Dato[];
 
-  constructor() { }
+  constructor(private cartService: CartService) {}
 
   ngOnInit(): void {
+    this.cartItemsInfo = [
+      {
+        name: 'Autito',
+        price: 200,
+        qty: 2,
+      },
+      {
+        name: 'Cactus',
+        price: 50,
+        qty: 5,
+      },
+      {
+        name: 'Zapas',
+        price: 2500,
+        qty: 1,
+      },
+    ];
   }
-
+}
+export class Dato {
+  name: string;
+  price: number;
+  qty: number;
 }
