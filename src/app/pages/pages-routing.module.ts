1f1952973e934e80f7232ed6555b3f331c863f18
Tomas Padilla <padillatomasagustin@gmail.com>
2022-03-07 14:25:27 +0100

    carPage - All Pages Created

--- src/app/pages/pages-routing.module.ts
+++ src/app/pages/pages-routing.module.ts
@@ -7,6 +7,10 @@ import { RegisterCustomComponent } from './register-custom/register-custom.compo
 import { ProductDetailsComponent } from './product-details/product-details.component';
 import { CreateProductComponent } from './create-product/create-product.component';
 import { CartComponent } from './cart/cart.component';
+import { CategoriesPageComponent } from './categories-page/categories-page.component';
+import { ProductsFilterComponent } from './products-filter/products-filter.component';
+import { UserProductsComponent } from './user-products/user-products.component';
+import { UserPurchasesComponent } from './user-purchases/user-purchases.component';
 //import { GuardService } from '../_service/guard.service';
 
 export const routes: Routes = [
@@ -17,6 +21,10 @@ export const routes: Routes = [
   { path: 'product-details/:id', component: ProductDetailsComponent },
   { path: 'create-product', component: CreateProductComponent },
   { path: 'cart', component: CartComponent },
+  { path: 'all-categories', component: CategoriesPageComponent },
+  { path: 'all-products', component: ProductsFilterComponent },
+  { path: 'user-products', component: UserProductsComponent },
+  { path: 'user-purchases', component: UserPurchasesComponent },
 ];
 
 @NgModule({
